1701f7ab996c32702f2f5a291c2964a5f079f936
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-18 21:13:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,8 @@ class SessionsController < ApplicationController
   def create
     if params[:name].nil?
       redirect_to login_path
+    elsif params[:name].empty?
+      redirect_to login_path
     else
       session[:name] = params[:name]
       redirect_to root_path
